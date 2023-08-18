c4d127bc99d63da4f4784d34e67a2fcca5f0774c
mrvzay <vzay1999@gmail.com>
2023-08-18 16:22:05 +0530

    add full name method

--- src/lecture/Main.java
+++ src/lecture/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Tim!");
+
+        fullName("A. ", "Vijay");
+    }
+
+    public static void fullName(String firstName, String secondName) {
+        System.out.println(firstName + secondName);
     }
 }
