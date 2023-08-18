6884e9a52a0942abf8f37905bea62f548139676a
mrvzay <115390140+mrvzay@users.noreply.github.com>
2023-08-18 16:26:20 +0530

    all the lines of code add comment

--- src/lecture/Main.java
+++ src/lecture/Main.java
@@ -2,14 +2,20 @@ package lecture;
 
 public class Main {
 
+    // Main method
     public static void main(String[] args) {
 
+        // write simple hello tim print line
         System.out.println("Hello, Tim!");
 
+        // calling the full name function
         fullName("A. ", "Vijay");
     }
 
+    // write method calld full name
     public static void fullName(String firstName, String secondName) {
+
+        // print full name
         System.out.println(firstName + secondName);
     }
 }
